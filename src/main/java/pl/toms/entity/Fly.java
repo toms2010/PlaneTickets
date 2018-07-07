0240efba40d8513b72fbda1ffec4421d96d74475
Michał Tomaszczak <m.tomaszczak@gmail.com>
2018-07-07 10:30:15 +0200

    Commit inicjujący

---
+++ src/main/java/pl/toms/entity/Fly.java
@@ -0,0 +1,16 @@
+package pl.toms.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Fly {
+	
+	id
+	departureAirport
+	arrivalAirport
+	departureDate
+	flyTime
+	PlaneTypeId
+	SeatsList
+
+}
---
+++ src/test/java/pl/toms/PlaneTicketsApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.toms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlaneTicketsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
